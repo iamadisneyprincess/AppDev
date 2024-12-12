f3b52beb4d183d13173a355a6f2456f10eb880c7
Angel <105346812+iamadisneyprincess@users.noreply.github.com>
2024-12-12 12:01:10 +0800

    gggg
    kugnbjhdgsgbfd

--- FLUTTER_2/app/src/main/java/com/example/cv/ui/Education/EducationFragment.kt
+++ FLUTTER_2/app/src/main/java/com/example/cv/ui/Education/EducationFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.cv.databinding.FragmentGalleryBinding
+import com.example.cv.databinding.FragmentEducationBinding
 
 class EducationFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentEducationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class EducationFragment : Fragment() {
         val educationViewModel =
             ViewModelProvider(this).get(EducationViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentEducationBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        val textView: TextView = binding.textEducation
         educationViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- FLUTTER_2/app/src/main/java/com/example/cv/ui/Education/EducationViewModel.kt
+++ FLUTTER_2/app/src/main/java/com/example/cv/ui/Education/EducationViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class EducationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is EDUCATION Fragment"
     }
     val text: LiveData<String> = _text
 }
